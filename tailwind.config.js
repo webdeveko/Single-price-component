43df9c607137408e09c5d1a7beb4c02a9d972863
Ekophe <genz.millenials@gmail.com>
2023-05-07 10:41:45 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "cyan": "#2bb3b1",
+        "light-cyan" : "#4abebd",
+        "bright-yellow": "#c0df34",
+        "light-gray": "#e5eff5",
+        "grayish-blue": "#98a6bd",
+      },
+      
+    },
+  },
+  plugins: [],
+}
+
